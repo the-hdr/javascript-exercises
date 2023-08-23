52d13467a5459c95277470f26f3dd8ab54297690
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-08-23 23:18:47 +0530

    Solve

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(originalString, iterationTimes)
+{
+    if (iterationTimes < 0)
+    {
+        return "ERROR";
+    }
 
+    let resultString = "";
+    for (let i = 0; i < iterationTimes; ++i)
+    {
+        resultString += originalString;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
