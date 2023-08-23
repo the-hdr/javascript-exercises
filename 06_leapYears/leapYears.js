c86a926db4408ac74695839bd2f4cb9c4bb61ada
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-08-24 00:27:59 +0530

    Solve
    Added solution to exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (y)
+{
+    if ('number' !== typeof (y) || y < 0)
+    {
+        return 'ERROR';
+    }
+    return ((y % 400 == 0) || (y % 100 != 0) && (y % 4 == 0));
 };
 
 // Do not edit below this line
