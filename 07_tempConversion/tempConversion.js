a3c19f8c3d840d819ebe545f2645ce2952fc5fb1
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-08-24 00:46:35 +0530

    Solve
    Added solution to exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (FahrenheitTemperature)
+{
+  if ('number' !== typeof FahrenheitTemperature)
+  {
+    return 'ERROR';
+  }
+  
+  return parseFloat(((FahrenheitTemperature - 32)*(5/9)).toFixed (1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature)
+{
+  if ('number' !== typeof celsiusTemperature)
+  {
+    return 'ERROR';
+  }
+
+  return parseFloat(((9/5)*celsiusTemperature + 32).toFixed (1));
 };
 
 // Do not edit below this line
