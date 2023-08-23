c830514cfdf3ee7ee8eacddf1ac0cd93026de5fe
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-08-24 00:22:51 +0530

    Solve
    Added solution to exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end)
+{
+    if ('number' !== typeof start || 'number' !== typeof end)
+    {
+        return "ERROR";
+    }
 
+    if (start > end)
+    {
+        let t = start;
+        start = end;
+        end = t;
+    }
+
+    if (start < 0)
+    {
+        return "ERROR";
+    }
+
+    let result = 0;
+    for (let currentNumber = start; currentNumber <= end; ++currentNumber)
+    {
+        result += currentNumber;
+    }
+    return result;
 };
 
 // Do not edit below this line
