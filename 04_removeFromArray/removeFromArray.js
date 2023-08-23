9d63f67bae99fd98e046bec652800c3b3d57a8f2
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-08-24 00:00:20 +0530

    Solve
    Added solution to exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ... illegalEntities)
+{
+    let resultArray = [];
+    let n = inputArray.length;
 
+    for (let i = 0; i < n; ++i)
+    {
+        let isIllegal = false;
+        for (const illegalEntity of illegalEntities)
+        {
+            if (illegalEntity === inputArray[i])
+            {
+                isIllegal = true;
+                break;
+            }
+        }
+        if (isIllegal == false)
+        {
+            resultArray.push (inputArray[i]);
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
